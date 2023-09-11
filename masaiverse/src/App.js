1be995ecb31e61cd20717adec0fec9abf3943e80
Sunil Chaudhary <sunilchaudhary7789@gmail.com>
2023-09-11 18:55:13 +0530

    featrue:added certification section

--- masaiverse/src/App.js
+++ masaiverse/src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import Curriculum from './components/Curriculum';
 import HiringPartner from './components/HiringPartner';
 import ElevateCarrier from './components/ElevateCarrier';
+import Certification from './components/Certification';
 
 function App() {
   return (
     <div>
-      
-      <ElevateCarrier/>
+      {/* <Curriculum/> */}
+      {/* <HiringPartner/> */}
+      {/* <ElevateCarrier/> */}
+      <Certification/>
     </div>
   );
 }
 
 export default App;
+
+
