92524d591e5fdb9cab0f490b299f82516b1733ef
Shivam Kumar <shivam2432000@gmail.com>
2023-11-16 11:26:25 +0530

    This is some changes done in news section

--- masaiverse/src/Components/News.jsx
+++ masaiverse/src/Components/News.jsx
@@ -2,11 +2,29 @@ import React from "react";
 import { Box, Heading, Image, Stack, Text } from "@chakra-ui/react";
 
 const News = () => {
+  const newsData = [
+    {
+      src: "https://masai-website-images.s3.ap-south-1.amazonaws.com/PR_Article_Prateek_Shukla_Gen_AI_92324e2d6b.jpeg",
+      alt: "img1",
+      text: "Empowering Education Through GenAI",
+    },
+    {
+      src: "https://masai-website-images.s3.ap-south-1.amazonaws.com/PR_Thungy_1_420b226e5b.webp",
+      alt: "img2",
+      text: "Masai School targets fivefold revenue growth in three years",
+    },
+    {
+      src: "https://masai-website-images.s3.ap-south-1.amazonaws.com/PR_Thingy_2_f2c52e6fcf.webp",
+      alt: "img3",
+      text: "Beyond Degrees: Embracing Alternative Credentialing and Micro-learning in Tech Recruitment",
+    },
+  ];
+
   return (
-    <Stack m={{ md: "25px", base: "15px" }}>
+    <Stack m={{ base: "15px", md: "25px", lg: "25px" }}>
       <Heading
         size="lg"
-        fontSize={{ md: "40px", base: "20px" }}
+        fontSize={{ base: "20px", md: "35px", lg: "40px" }}
         textAlign="center"
       >
         Masai In <span style={{ color: "red" }}>News</span>
@@ -15,63 +33,35 @@ const News = () => {
         display="flex"
         width="90%"
         margin="auto"
-        flexDirection={{ md: "row", base: "column" }}
+        flexDirection={{ base: "column", md: "column", lg: "row" }}
       >
-        <Box boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px" margin="10px">
-          <Image
-           display={{base:"block"}}
-           m={{base:"auto"}}
-            src="https://masai-website-images.s3.ap-south-1.amazonaws.com/PR_Article_Prateek_Shukla_Gen_AI_92324e2d6b.jpeg"
-            alt="img1"
-            height="250px"
-            width={"600px"}
-          />
-          <Text
-            fontSize={{ md: "22px", base: "15px" }}
-            fontWeight="bold"
-            m="12px"
+        {newsData.map((item, index) => (
+          <Box
+            key={index}
+            margin="10px"
+            borderRadius={"12px"}
+            width={{ base: "95%", md: "95%", lg: "33%" }}
+            boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px"
           >
-            Empowering Education Through GenAI
-          </Text>
-        </Box>
-
-        <Box boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px" margin="10px">
-          <Image
-          display={{base:"block"}}
-          m={{base:"auto"}}
-            src="https://masai-website-images.s3.ap-south-1.amazonaws.com/PR_Thungy_1_420b226e5b.webp"
-            alt="img1"
-            height="250px"
-            width={"600px"}
-          />
-          <Text
-            fontSize={{ md: "22px", base: "15px" }}
-            fontWeight="bold"
-            m="12px"
-          >
-            Masai School targets fivefold revenue growth in three years
-          </Text>
-        </Box>
-
-        <Box boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px" margin="10px">
-          <Image
-          display={{base:"block"}}
-          m={{base:"auto"}}
-            src="https://masai-website-images.s3.ap-south-1.amazonaws.com/PR_Thingy_2_f2c52e6fcf.webp"
-            alt="img1"
-            height="250px"
-            width={"600px"}
-            objectFit={"cover"}
-          />
-          <Text
-            fontSize={{ md: "22px", base: "15px" }}
-            fontWeight="bold"
-            m="12px"
-          >
-            Beyond Degrees: Embracing Alternative Credentialing and
-            Micro-learning in Tech Recruitment
-          </Text>
-        </Box>
+            <Image
+              display={"block"}
+              m={"auto"}
+              src={item.src}
+              alt={item.alt}
+              height="60%"
+              width="100%"
+              objectFit="cover"
+            />
+            <Text
+              fontSize={{ base: "15px", md: "22px", lg: "22px" }}
+              fontWeight="bold"
+              textAlign={{ base: "center", md: "center", lg: "left" }}
+              m="12px"
+            >
+              {item.text}
+            </Text>
+          </Box>
+        ))}
       </Stack>
     </Stack>
   );
